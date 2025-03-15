aca9299b70cff7dc403ef5f7852d9975ca797539
SAHITHI <sahithidornala.1999@gmail.com>
2025-03-15 12:30:26 +0000

    sixth problem

---
+++ prob6.py
@@ -0,0 +1,11 @@
+number_x=list(input("Enter the list:"))
+print(number_x)
+length_list= len(number_x)
+print(length_list)
+if(number_x[0] == number_x[length_list-1]):
+    print("Result is True")
+else:
+    print("Result is False")
+
+
+
