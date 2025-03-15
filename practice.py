d14b6c227ea4c647b6268f6b0dea27454d1da8f7
SAHITHI <sahithidornala.1999@gmail.com>
2025-03-15 11:59:25 +0000

    general practice

---
+++ practice.py
@@ -0,0 +1,42 @@
+#using input
+#name = input("enter name:")
+#print (name)
+#using print function
+print("Hello sahithi")
+#Python is dynamically typed which means it will execute line by line
+a=3
+print(a)
+a=2
+print(a)
+b="string"
+print(b)
+c=1.00
+print(c)#these all are basic operations
+print(type(c))
+a= "sahithi"
+print("welcome",a)
+a=6
+print("sahithi","value please :",a)
+#using end function for
+print("welcome",end="")
+print(" mommy")
+#using sep function for seperation
+print("08","12","1999",sep="-")
+#list
+my_list="sahithi"
+for i in my_list:
+    print(i)
+
+a=4
+my_lista=[1, "sunny", a, 8]
+my_listb=[2,my_lista]
+print(my_listb)
+for i in my_listb:
+    print (i)
+
+print(len(my_lista))
+my_a=[1,3,4,8]
+my_b=[2,4,1,7]
+my_c=my_a+my_b
+print(my_c)
+
