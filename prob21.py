89bc4d5f711c17100218120324621e080c279f34
SAHITHI <sahithidornala.1999@gmail.com>
2025-04-26 09:38:39 +0100

    21st problem

---
+++ prob21.py
@@ -0,0 +1,8 @@
+str1 = "Pynative123Python"
+for i in str1:
+    if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        print("String has numbers")
+        break
+else:
+    print("String does not have numbers")
+
