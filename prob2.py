1393849d19ff441eb1fc80354f8b17e4db52a5ec
SAHITHI <sahithidornala.1999@gmail.com>
2025-05-05 14:40:00 +0100

    2nd problem

---
+++ prob2.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number:"))
+print("Printing current and previous number sum in a range",a)
+
+for i in range (0,a):
+    pv=i-1
+    if pv<0:
+        pv=0
+    sum=i+pv
+    print("current number",i,"previous number",pv, "sum:",sum)
+
