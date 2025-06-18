560e4f5a2c4174674c170936e1dbe2f27bb32a35
SAHITHI <sahithidornala.1999@gmail.com>
2025-06-18 20:11:21 +0100

    prime numbers

---
+++ prob19.py
@@ -0,0 +1,9 @@
+list_1=[]
+for i in range(2,20):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        list_1.append(i)
+print("All prime numbers from 1 to 20:",list_1)
+print("Alternate prime numbers from 1 to 20:",list_1[0::2])
