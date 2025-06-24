bd9f8f0180d1462bf3c9a22bc47e33feea55da28
SAHITHI <sahithidornala.1999@gmail.com>
2025-06-24 17:19:22 +0100

    income sum

---
+++ prob12.py
@@ -0,0 +1,14 @@
+income=45000
+ori=income
+if ori>10000:
+    income=income-10000
+if ori<20000 and ori>10000:
+    tax=income*0.10
+    print("tax:",tax)
+elif ori>20000:
+    t=income-10000
+    st=(t*0.20)+(10000*0.10)
+    print("tax:",st)
+else:
+    print("no tax")
+
